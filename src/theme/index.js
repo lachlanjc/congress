eecd47734ea5db3f85226c56324be9726baae4b7
Lachlan Campbell <lachlan.campbell@icloud.com>
2018-02-17 13:40:11 -0500

    add gt america font

--- src/theme/index.js
+++ src/theme/index.js
@@ -3,6 +3,42 @@ import { injectGlobal, ThemeProvider as Root } from 'styled-components'
 import theme from './config'
 
 injectGlobal`
+  @font-face {
+    src: url('/gt-america-ultralight.woff') format('woff');
+    font-family: GT-America;
+    font-weight: 100;
+  }
+  @font-face {
+    src: url('/gt-america-thin.woff') format('woff');
+    font-family: GT-America;
+    font-weight: 200;
+  }
+  @font-face {
+    src: url('/gt-america-light.woff') format('woff');
+    font-family: GT-America;
+    font-weight: 300;
+  }
+  @font-face {
+    src: url('/gt-america-regular.woff') format('woff');
+    font-family: GT-America;
+    font-weight: 400;
+  }
+  @font-face {
+    src: url('/gt-america-medium.woff') format('woff');
+    font-family: GT-America;
+    font-weight: 500;
+  }
+  @font-face {
+    src: url('/gt-america-bold.woff') format('woff');
+    font-family: GT-America;
+    font-weight: 700;
+  }
+  @font-face {
+    src: url('/gt-america-black.woff') format('woff');
+    font-family: GT-America;
+    font-weight: 900;
+  }
+
   body {
     background-color: ${theme.colors.snow};
     font-family: ${theme.font};

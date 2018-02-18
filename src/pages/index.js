6b4f5ad88cbba403903d7d5a8df21a1aae899033
Lachlan Campbell <lachlan.campbell@icloud.com>
2018-02-17 20:00:20 -0500

    design header

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import Link from 'gatsby-link'
 import Helmet from 'react-helmet'
 import {
   Container,
+  BackgroundImage,
   Heading,
   Text,
   Button,
@@ -11,21 +12,37 @@ import {
 } from '@hackclub/design-system'
 import Search from 'components/Search'
 
+const Mark = Text.withComponent('mark').extend`
+  background: transparent url(/underline.svg) bottom left no-repeat;
+  background-size: 100% 0.5rem;
+  padding-bottom: 0.25rem;
+  color: inherit;
+`
+
 export default () => (
   <main>
-    <Box bg="primary">
-      <Container w={1} maxWidth={36} py={5}>
-        <Heading.h1 color="white" f={[5, 6]} mt={0}>
-          Congress
-        </Heading.h1>
-        <Text color="white" f={3} my={3}>
-          Work in progress
-        </Text>
+    <BackgroundImage
+      image="/hero.jpg"
+      bg="primary"
+      py={[5, 6]}
+      px={3}
+      align="center"
+    >
+      <Heading.h1 color="white" f={[5, 6]} mt={0}>
+        Let’s <Mark>take back</Mark> Congress.
+      </Heading.h1>
+      <Container w={1} maxWidth={36}>
+        <Heading.h2 color="white" f={[3, 4]} my={3} regular>
+          Our members of Congress work for us, the people. But they’re only
+          listening to their big donors.
+          <br />
+          <strong>Make your voice heard.</strong>
+        </Heading.h2>
       </Container>
-    </Box>
+    </BackgroundImage>
     <Container w={1} maxWidth={36} py={4}>
-      <Heading.h2 f={2} color="muted" caps regular mb={2}>
-        Your Representative
+      <Heading.h2 f={4} mb={2}>
+        Find your members of Congress
       </Heading.h2>
       <Search />
     </Container>

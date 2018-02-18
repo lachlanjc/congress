4cc9c884f86c94aa7d90a945e66fa5971c6c79f1
Lachlan Campbell <lachlan.campbell@icloud.com>
2018-02-17 23:20:59 -0500

    improve downloader

--- data/download.js
+++ data/download.js
@@ -1,16 +1,31 @@
-const { map, forEach, find, last, toNumber } = require('lodash')
+const { map, forEach, find, last, toNumber, kebabCase } = require('lodash')
 const fs = require('fs')
 const axios = require('axios')
 
 const USIO = 'https://theunitedstates.io/congress-legislators'
 
-const getProfiles = axios
-  .get(`${USIO}/legislators-current.json`)
-  .then(res => res.data)
+const getProfiles = axios.get(`${USIO}/legislators-current.json`).then(res => {
+  console.log('🛬 downloaded main list')
+  return res.data
+})
 const getAccounts = axios
   .get(`${USIO}/legislators-social-media.json`)
-  .then(res => res.data)
+  .then(res => {
+    console.log('🍭 downloaded social media')
+    return res.data
+  }) /*
     })
+*/
+const makeId = profile => {
+  const { state } = last(profile.terms)
+  if (profile.chamber === 'rep') {
+    let { district } = profile.term
+    if (district.toString().length === 1) district = `0${district}`
+    return `${state}-${district}`
+  } else {
+    return `${state}-${kebabCase(profile.name.official_full)}`
+  }
+}
 const getPeople = () =>
   Promise.all([getProfiles, getAccounts])
     .catch(() => {
@@ -19,18 +34,22 @@ const getPeople = () =>
     })
     .then(([profiles, accounts]) => {
       forEach(profiles, profile => {
-        // reduce data
+        // set attributes
+        profile.term = last(profile.terms)
+        profile.chamber = profile.term.type
         const { opensecrets, bioguide } = profile.id
         profile.ids = { opensecrets, bioguide }
-        delete profile.id
+        profile.id = makeId(profile)
+        // reduce data size
         profile.gender = profile.bio.gender
         delete profile.bio
-        profile.term = last(profile.terms)
-        delete profile.term.rss_url
-        delete profile.term.state_rank
-        delete profile.term.class
-        delete profile.terms
         delete profile.leadership_roles
+        delete profile.term.state_rank
+        delete profile.term.rss_url
+        delete profile.term.class
+        delete profile.term.type
+        delete profile.term.rep
+        delete profile.terms
         // attach social
         const account = find(accounts, ['id.bioguide', bioguide])
         if (account) profile.social = account.social

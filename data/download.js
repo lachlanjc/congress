f4220e703a15c9246443fe4d95b13a777d33489d
Lachlan Campbell <lachlan.campbell@icloud.com>
2018-02-24 15:25:30 -0500

    rewrite download

--- data/download.js
+++ data/download.js
@@ -1,123 +1,99 @@
-const {
-  map,
-  forEach,
-  find,
-  last,
-  toNumber,
-  kebabCase,
-  isEmpty,
-  filter,
-  pick,
-  set,
-  includes
-} = require('lodash')
+const { map, find, last, toNumber, isEmpty, filter, pick } = require('lodash')
 const fs = require('fs')
 const axios = require('axios')
 const neatCsv = require('neat-csv')
 const pBreak = require('p-break')
-const pSeries = require('p-series')
+// const pSeries = require('p-series')
 const pWaterfall = require('p-waterfall')
 const writeJsonFile = require('write-json-file')
-
+const retry = require('async-retry')
 const USIO = 'https://theunitedstates.io/congress-legislators'
 
 const getProfiles = () =>
-  axios.get(`${USIO}/legislators-current.json`).then(res => {
+  axios(`${USIO}/legislators-current.json`).then(res => {
     console.log('🛬 downloaded main list')
     return res.data
   })
 const getAccounts = () =>
-  axios.get(`${USIO}/legislators-social-media.json`).then(res => {
+  axios(`${USIO}/legislators-social-media.json`).then(res => {
     console.log('🍭 downloaded social media')
     return res.data
   })
-const makeId = profile => {
-  const { state } = last(profile.terms)
-  if (profile.chamber === 'rep') {
-    let { district } = profile.term
+const makeId = term => {
+  if (term.type === 'rep') {
+    let { district } = term
     if (district.toString().length === 1) district = `0${district}`
-    return `${state}-${district}`
-  } else {
-    return `${state}-${kebabCase(profile.name.official_full)}`
+    return `${term.state}-${district}`
+  } else if (term.type === 'sen') {
+    return `${term.state}-sen-${term.class}`
   }
 }
 const getBaseData = () =>
-  pSeries([() => getProfiles(), () => getAccounts()])
+  Promise.all([getProfiles(), getAccounts()])
     .catch(() => {
       console.error('🚨 ERROR GETTING PEOPLE')
     })
     .then(([profiles, accounts]) =>
-      forEach(profiles, profile => {
-        // set attributes
-        profile.term = last(profile.terms)
-        profile.chamber = profile.term.type
-        profile.gender = profile.bio.gender
-        const { opensecrets, bioguide } = profile.id
-        profile.ids = { opensecrets, bioguide }
-        profile.id = makeId(profile)
-        // attach social
-        const account = find(accounts, ['id.bioguide', bioguide])
-        if (account) {
-          profile.social = pick(account.social, ['twitter', 'facebook'])
+      map(profiles, async function(profile) {
+        const term = last(profile.terms)
+        const account = find(accounts, ['id.bioguide', profile.id.bioguide])
+        const contribs = await getContribs(profile.id.opensecrets)
+        return {
+          id: makeId(term),
+          name: {
+            first: profile.name.first,
+            last: profile.name.last,
+            full: profile.name.official_full
+          },
+          contribs,
+          role: term.type,
+          state: term.state,
+          termStart: term.start,
+          termEnd: term.end,
+          gender: profile.bio.gender,
+          ids: pick(profile.id, ['opensecrets', 'bioguide']),
+          contact: {
+            form: term.contact_form,
+            phone: term.phone,
+            url: term.url,
+            ...(!isEmpty(account) &&
+              pick(account.contact, ['twitter', 'facebook']))
+          }
         }
-        return profile
       })
     )
-const reduceData = profile => {
-  delete profile.bio
-  delete profile.family
-  delete profile.other_names
-  delete profile.leadership_roles
-  if (profile.term) {
-    delete profile.term.state_rank
-    delete profile.term.address
-    delete profile.term.rss_url
-    delete profile.term.class
-    delete profile.term.type
-    delete profile.term.rep
-  }
-  delete profile.terms
-  return profile
-}
-const addContribs = profile => {
-  const url =
-    `https://www.opensecrets.org/members-of-congress/contributors.csv` +
-    `?cid=${profile.ids.opensecrets}&cycle=2016&type=C`
-  const makeRecord = record => ({
-    name: record.ultorg,
-    total: toNumber(record.total)
-  })
-  return pWaterfall([
-    init =>
-      axios(url)
-        .then(res => res.data)
-        .catch(err => {
-          console.error('⛔️', url)
-        }),
-    data =>
-      isEmpty(data)
-        ? profile
-        : neatCsv(data)
-            .then(data => map(data, makeRecord))
-            .then(data => {
-              profile.contribs = data
-              process.stdout.write(`${profile.id} – `)
-              return profile
-            })
-  ])
+
+const contribsUrl = id =>
+  `https://www.opensecrets.org/members-of-congress/contributors.csv` +
+  `?cid=${id}&cycle=2016&type=C`
+const makeContribRecord = record => ({
+  name: record.ultorg,
+  total: toNumber(record.total)
+})
+const processContribs = data =>
+  neatCsv(data).then(data => map(data, makeContribRecord))
+async function getContribs(id) {
+  await retry(
+    async bail => {
+      const res = await axios(contribsUrl(id))
+
+      if (res.status !== 200) {
+        bail(new Error(`${res.status} error for ${id}`))
+        return
+      } else {
+        process.stdout.write(id + ' – ')
+      }
+
+      const data = await res.body
+      return isEmpty(data) ? [] : processContribs(data)
+    },
+    {
+      retries: 8
+    }
+  )
 }
 const save = data => {
-  fs.writeFile('./data/people.json', JSON.stringify(data), err => {
-    if (err) {
-      console.error('🚨 ERROR SAVING FILE', err)
-    } else {
-      console.log(`\n✅ Saved ${data.length} people`)
-    }
-  })
+  writeJsonFile.sync('./data/people.json', data)
+  console.log(`\n✅ Saved ${data.length} people`)
 }
-pWaterfall([
-  init => getBaseData(),
-  baseData => map(baseData, addContribs),
-  fullData => map(fullData, reduceData),
-  data => save(data)
-])
+getBaseData().then(finalData => save(finalData))

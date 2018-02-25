55a7d84c0b4b54b2a7952eabc09a010071432b4d
Lachlan Campbell <lachlan.campbell@icloud.com>
2018-02-25 18:07:39 -0500

    update dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,13 +1,5 @@
 module.exports = {
   plugins: [
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        path: `${__dirname}/data`,
-        name: 'people'
-      }
-    },
-    'gatsby-transformer-json',
     'gatsby-plugin-resolve-src',
     'gatsby-plugin-react-next',
     'gatsby-plugin-react-helmet',

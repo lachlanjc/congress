ec2ebb17467b8367352960ed20eb08b45d3aac04
Lachlan Campbell <lachlan.campbell@icloud.com>
2018-02-25 17:58:40 -0500

    download contribs

---
+++ data/add-contribs.js
@@ -0,0 +1,16 @@
+const { map, forEach } = require('lodash')
+const writeJsonFile = require('write-json-file')
+const loadJsonFile = require('load-json-file')
+
+const process = profile => {
+  const id = profile.ids.opensecrets
+  const contribs = loadJsonFile.sync(`./data/contribs/${id}.json`)
+  return {
+    ...profile,
+    contribs
+  }
+}
+
+loadJsonFile('./data/people.json')
+  .then(profiles => map(profiles, p => process(p)))
+  .then(data => writeJsonFile('./data/data.json', data))
--- data/download.js
+++ data/download.js
@@ -1,13 +1,9 @@
 const { map, find, last, toNumber, isEmpty, filter, pick } = require('lodash')
 const fs = require('fs')
 const axios = require('axios')
-const got = require('got')
 const neatCsv = require('neat-csv')
-const pBreak = require('p-break')
-// const pSeries = require('p-series')
-const pWaterfall = require('p-waterfall')
 const writeJsonFile = require('write-json-file')
-const retry = require('async-retry')
+
 const USIO = 'https://theunitedstates.io/congress-legislators'
 
 const getProfiles = () =>
@@ -31,38 +27,8 @@ const makeId = term => {
     return `${term.state}-sen-${term.class}`
   }
 }
-const contribsUrl = id =>
-  `https://www.opensecrets.org/members-of-congress/contributors.csv` +
-  `?cid=${id}&cycle=2016&type=C`
-const makeContribRecord = record => ({
-  name: record.ultorg,
-  total: toNumber(record.total)
-})
-const processContribs = data =>
-  neatCsv(data).then(data => map(data, makeContribRecord))
-async function getContribs(id) {
-  await retry(
-    async bail => {
-      const res = await axios(contribsUrl(id))
-
-      if (res.status !== 200) {
-        bail(new Error(`${res.status} error for ${id}`))
-        return
-      } else {
-        process.stdout.write(id + ' – ')
-      }
-
-      const data = await res.body
-      return isEmpty(data) ? [] : processContribs(data)
-    },
-    {
-      retries: 8
-    }
-  )
-}
-async function processProfile(profile, account) {
+const processProfile = (profile, account) => {
   const term = last(profile.terms)
-  const contribs = await getContribs(profile.id.opensecrets)
   return {
     id: makeId(term),
     name: {
@@ -70,7 +36,6 @@ async function processProfile(profile, account) {
       last: profile.name.last,
       full: profile.name.official_full
     },
-    contribs,
     role: term.type,
     state: term.state,
     termStart: term.start,
@@ -85,18 +50,17 @@ async function processProfile(profile, account) {
     }
   }
 }
-async function getData() {
-  return await Promise.all([getProfiles(), getAccounts()])
+const getData = () =>
+  Promise.all([getProfiles(), getAccounts()])
     .catch(() => {
       console.error('🚨 ERROR GETTING PEOPLE')
     })
     .then(([profiles, accounts]) =>
       map(profiles, p => processProfile(p, findAccount(p, accounts)))
     )
-}
 const save = data => {
-  writeJsonFile.sync('./data/people.json', data)
-  console.log(`\n✅ Saved ${data.length} people`)
+  writeJsonFile('./data/people.json', data).then(() => {
+    console.log(`\n✅ Saved ${data.length} people`)
+  })
 }
-
 getData().then(data => save(data))
